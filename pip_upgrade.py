2a2978734020c994516747e4bbcd30d428448698
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2023-06-04 19:40:41 +0200

    upgrade one by one; first check pip is upgraded; no error

--- pip_upgrade.py
+++ pip_upgrade.py
@@ -2,6 +2,7 @@
 
 import subprocess
 
+subprocess.run(['pip', 'install', 'pip', '--upgrade'], check=False)
 """find list of packages"""
 pakages = subprocess.run(["pip", "list"], stdout=subprocess.PIPE,
                          text=True, check=False)
@@ -15,33 +16,33 @@ while i < len(vlist):
     del vlist[i]
     i += 1
 
-"""upgrade packages all togheder"""
-command = ["pip", "install"]
-command = command + vlist
-command = command + ["-U"]
-upgrade = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+"""upgrade packages one by one"""
+for i in vlist:
+    print(f"\n\n\tI'm checking to upgrade {i}")
+    command = ["pip", "install", '--upgrade', i]
+    upgrade = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                          text=True, check=False)
-print(upgrade.stdout)
+    print(upgrade.stdout)
 
-"""manage errors"""
-exception = ["\n\n",
-            "*********************************\n",
-            "\n*********************************",
-            "I was not be able to upgrade these packages:\t"]
-while upgrade.stderr != '':
-    error_str = upgrade.stderr
-    error_list = error_str.split()
-    error_package = error_list[3]
-    error_package = error_package.replace('\'', '')
-    error_package = error_package.replace('.', '')
-    command.remove(error_package)
-    error_package += ", "
-    exception = exception + [error_package]
-    upgrade = subprocess.run(command, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, text=True, check=False)
+# """manage errors"""
+# exception = ["\n\n",
+#             "*********************************\n",
+#             "\n*********************************",
+#             "I was not be able to upgrade these packages:\t"]
+# while upgrade.stderr != '':
+#     error_str = upgrade.stderr
+#     error_list = error_str.split()
+#     error_package = error_list[3]
+#     error_package = error_package.replace('\'', '')
+#     error_package = error_package.replace('.', '')
+#     command.remove(error_package)
+#     error_package += ", "
+#     exception = exception + [error_package]
+#     upgrade = subprocess.run(command, stdout=subprocess.PIPE,
+#                              stderr=subprocess.PIPE, text=True, check=False)
 
-print(upgrade.stdout)
+# print(upgrade.stdout)
 
-if len(exception) > 4:
-    for element in exception:
-        print(element)
+# if len(exception) > 4:
+#     for element in exception:
+#         print(element)
