ce12019aa293d9fc3d89a54e995acbc4c64a9def
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-04-14 18:44:59 +0200

    some test about multiprocessing

---
+++ multi.py
@@ -0,0 +1,19 @@
+import multiprocessing as mp
+from datetime import datetime as dt
+from time import sleep
+
+def fun(i) :
+  with open(f'/Users/Alpha/{i}.txt', 'w') as file:
+    file.write(f'File {i}')
+  sleep(2)
+
+if __name__ == "__main__" :
+  NUM = 16
+  pool = mp.Pool(NUM)
+  START = dt.now()
+  pool.map(fun, range(NUM))
+  pool.close()
+  pool.join()
+  END = dt.now()
+  TIME = END - START
+  print(f"Tempo:\t{TIME}")
