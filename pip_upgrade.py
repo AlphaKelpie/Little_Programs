1d7ebff3776691d177aac41d708babc3db117e3f
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-06-24 11:38:43 +0200

    manage slow/heavy upgrades

--- pip_upgrade.py
+++ pip_upgrade.py
@@ -9,6 +9,7 @@ with open('not_upgrade_pip.txt', 'r') as file:
     for line in lines:
         not_upgrade.append(line.split(' ')[0])
 
+FAT = ['grpcio', 'opencv-python']
 
 #upgrade pip
 print("\tI upgrade pip")
@@ -30,22 +31,25 @@ while i < len(vlist):
 
 #upgrade packages one by one
 for i in vlist:
-    print(f"\n\n\033[1;38;5;208mI'm checking to upgrade {i}\033[0;0m")
+    print(f"\n\033[1;38;5;208mI'm checking to upgrade {i}\033[0;0m")
     if i in not_upgrade:
         print(f"\033[0;31m{i} will not be upgraded\033[0;0m")
         continue
     command = ["pip", "install", '--upgrade', i]
-    upgrade = run(command, stdout=PIPE, text=True, check=True)
-    out = upgrade.stdout
-    outs = out.split('\n')
-
-    del outs[-1]
-    already_update = all("Requirement already satisfied:" in s for s in outs)
-    if already_update:
-        print(f"\033[1;36m{i} is already updated\033[0;0m\n")
+    if i in FAT:
+        upgrade = run(command, text=True, check=False)
     else:
-        # print('\n'.join(str(i) for i in outs))
-        print(f"\033[1;32m{outs[-1]}\033[0;0m\n")
+        upgrade = run(command, stdout=PIPE, text=True, check=True)
+        out = upgrade.stdout
+        outs = out.split('\n')
+
+        del outs[-1]
+        already_update = all("Requirement already satisfied:" in s for s in outs)
+        if already_update:
+            print(f"\033[1;36m{i} is already updated\033[0;0m\n")
+        else:
+            # print('\n'.join(str(i) for i in outs))
+            print(f"\033[1;32m{outs[-1]}\033[0;0m\n")
 
 
 #clean cache
